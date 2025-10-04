7694ca54bfd7f6a6aa632fa63855fedd12c4ac47
VGCoding <vgcoding05@gmail.com>
2025-10-04 11:25:12 +0530

    Deploy changes part 2

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
-require("dotenv").config()
+require("dotenv").config();
 const app = require("./src/app");
 
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
-});
+// Export the app for serverless deployment
+module.exports = app;
