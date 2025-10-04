9ae56128e7b2539e559cd76b242ecbc38ef72365
VGCoding <vgcoding05@gmail.com>
2025-10-04 11:32:24 +0530

    Deploy changes part 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // Ensure proper base path for deployment
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
 });
